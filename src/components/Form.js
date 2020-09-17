9cf53d6dc7d3f99ecd60164719ded3b60441a073
valentin-muller <58423332+valentin-muller@users.noreply.github.com>
2020-09-17 15:43:46 +0200

    setStatus issue

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,17 @@
 import React from 'react'
 
-const Form = ({ inputText, setInputText, todos, setTodos }) => {
+const Form = ({ inputText, setInputText, todos, setTodos, setStatus }) => {
     const inputTextHandler = (e) => {
         setInputText(e.target.value);
-    }
+    };
     const submitToDoHandler = (e) => {
         e.preventDefault();
         setTodos([...todos, {text: inputText, completed: false, id: Math.random() * 1000 }]);
         setInputText("");
-    }
+    };
+    const statusHandler = (e) => {
+        setStatus(e.target.value);
+    };
     return (
         <div>
             <form>
@@ -17,7 +20,7 @@ const Form = ({ inputText, setInputText, todos, setTodos }) => {
                     <i className="fas fa-plus-square"></i>
                 </button>
                 <div className="select">
-                    <select name="todos" className="filter-todo">
+                    <select onChange={statusHandler} name="todos" className="filter-todo">
                         <option value="all">All</option>
                         <option value="completed">Completed</option>
                         <option value="uncompleted">Uncompleted</option>
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -6,13 +6,22 @@ const Todo = ({ text, todos, todo, setTodos }) => {
     const deleteHandler = () => {
          setTodos(todos.filter((el) => el.id !== todo.id));
     };
-
+    const completeHandler = () => {
+        setTodos(todos.map((el) => {
+            if(el.id === todo.id) {
+                return {
+                    ...el, completed: !el.completed
+                }
+            }
+            return el;
+        }))
+    }
 
     return (
       <div className="todo">
 
-        <li className="todo-item">{text}</li>
-        <button className="complete-btn">
+        <li className={`todo-item ${todo.completed ? "completed" : ''}`}>{text}</li>
+        <button onClick={completeHandler} className="complete-btn">
           <i className="fas fa-check"></i>
         </button>
 
