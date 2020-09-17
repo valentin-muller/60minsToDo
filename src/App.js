8a27151352f0c1af37cb14d91995067c38fb52dd
valentin-muller <58423332+valentin-muller@users.noreply.github.com>
2020-09-17 16:36:22 +0200

    filterHander function add

--- src/App.js
+++ src/App.js
@@ -4,13 +4,40 @@ import Form from "./components/Form";
 import ToDoList from "./components/ToDoList";
 
 function App() {
+
+  //States
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  //Functions
+  const filterHandler = () => {
+    switch (status) {
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true));
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  }
   return (
     <div className="App">
-      <h1>Hello {inputText}</h1>
-      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
-      <ToDoList setTodos={setTodos} todos={todos} />
+      <Form 
+      inputText={inputText} 
+      todos={todos} 
+      setTodos={setTodos} 
+      setInputText={setInputText}
+      setStatus={setStatus}
+      />
+      <ToDoList 
+      setTodos={setTodos} 
+      todos={todos} 
+      />
     </div>
   );
 }
